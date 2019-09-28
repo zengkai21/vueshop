68ae632d7dd40fe0f1ffaf0b723268ff7b255505
zengxiaobin <1534805557@qq.com>
2019-09-28 16:47:17 +0800

    第二天修改新闻与评论

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -9,28 +9,28 @@
     <!-- 六宫格区域 -->
     <ul class="mui-table-view mui-grid-view mui-grid-9">
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to="/home/newslist">
           <img src="../../images/menu1(1).png" alt="">
           <div class="mui-media-body">新闻资讯</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to="">
           <img src="../../images/menu2.png" alt="">
           <div class="mui-media-body">图片分享</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to="">
           <img src="../../images/menu3.png" alt="">
           <div class="mui-media-body">商品购买</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
+        <router-link to="">
           <img src="../../images/menu4.png" alt="">
           <div class="mui-media-body">留言反馈</div>
-        </a>
+        </router-link>
       </li>
       <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
         <a href="#">
@@ -65,7 +65,7 @@ export default {
   methods: {
     getlunbotu() {
       this.$http
-        .get("http://www.liulongbin.top:3005/api/getlunbo")
+        .get("api/getlunbo")
         .then(result => {
           if (result.body.status === 0) {
             //success
--- src/main.js
+++ src/main.js
@@ -5,10 +5,20 @@ import VueRouter from 'vue-router'
 //1.2安装路由
 Vue.use(VueRouter)
 
+
+//导入格式化时间的插件
+import moment from 'moment'
+//定义全局过滤器
+Vue.filter('dateFormat',function(datestr,pattern='YYYY-MM-DD HH:mm:ss'){
+    return moment(datestr).format(pattern)
+})
+
+
 //导入vue-resource
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
-
+//设置请求根路径
+Vue.http.options.root="http://www.liulongbin.top:3005"
 //导入App根组件
 import app from './App.vue'
 
@@ -18,11 +28,12 @@ import './lib/mui/css/icons-extra.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 //引入 Mint-UI 的 Header 组件
-import { Header,Swipe, SwipeItem  } from 'mint-ui';
+import { Header,Swipe, SwipeItem,Button } from 'mint-ui';
 
 Vue.component(Header.name, Header);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Button.name, Button);
 
 //1.3导入自己的router.js 路由模块
 import router from './router.js'
